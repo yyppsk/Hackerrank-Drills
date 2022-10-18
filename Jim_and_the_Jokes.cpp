eb5a0e60a50db7e092bcc92106ff29f7e150ade6
yyppsk <69805496+yyppsk@users.noreply.github.com>
2022-10-18 10:23:15 +0530

    Jim and the jokes

---
+++ Jim_and_the_Jokes.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+/*
+ * Complete the 'solve' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts 2D_INTEGER_ARRAY dates as parameter.
+ */
+
+int solve(vector<vector<int>> dates)
+{
+}
+
+int main()
+{
+}
